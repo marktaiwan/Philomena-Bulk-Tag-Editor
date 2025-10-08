d5584264e6e29b3a76d572041611e05b4bac2de7
Mark Taiwan <marktaiwan@users.noreply.github.com>
2025-10-08 08:57:50 +0800

    Suppress typescript error

--- src/main.ts
+++ src/main.ts
@@ -133,7 +133,7 @@ function insertBulkUI(): void {
   editor.style.marginTop = '10px';
   editor.classList.add('layout--narrow', 'hidden');
   imageListHeader.append(toggleButton);
-  imageListHeader.parentElement.after(editor);
+  imageListHeader.parentElement!.after(editor);
 }
 
 function toggleUI(): void {
